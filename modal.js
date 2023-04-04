1880da93021d294b2103342377aac77dbb94b2ed
EJ <eric_j_fra@yahoo.fr>
2023-04-04 10:03:41 +0200

    2 First issue/fermeture de la modale

--- modal.js
+++ modal.js
@@ -8,16 +8,26 @@ function editNav() {
 }
 
 // DOM Elements
-const modalbg = document.querySelector(".bground");
+const modalBg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const modalClose = document.querySelectorAll(".close")
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
 // launch modal form
 function launchModal() {
-  modalbg.style.display = "block";
+  modalBg.style.display = "block";
+}
+
+//close modal event
+modalClose.forEach((toto) =>toto.addEventListener("click",closeModal));
+
+
+//close modal form
+function closeModal(){
+  modalBg.style.display = "none";
 }
 
 
